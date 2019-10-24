12f608b021e17e662dc77eee7891434b35001a86
Zubair <zubair1024@gmail.com>
2019-10-24 21:26:22 +0400

    adding in necessary controls

--- app/src/components/Map/Map.js
+++ app/src/components/Map/Map.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Map.css";
 
+// import groupToRegions from "../../rawdata/postCodeWithRegions.json";
+
 class MapComponent extends Component {
   state = {};
   render() {
@@ -10,10 +12,13 @@ class MapComponent extends Component {
       </div>
     );
   }
+
   componentDidMount() {
     this.onLoad();
     setTimeout(() => {
       this.renderE5Engineers();
+      // this.renderBoilerJobs();
+      // this.renderServiceJobs();
     }, 3000);
   }
 
@@ -36,49 +41,194 @@ class MapComponent extends Component {
     });
   }
 
+  getBoilerJobs() {
+    return new Promise((resolve, reject) => {
+      import("../../rawdata/boilerJobs.json")
+        .then(data => {
+          if (data && data.default) {
+            const validatedData = data.default.filter(item => {
+              return item.lat && item.lng;
+            });
+            resolve(validatedData);
+          } else {
+            resolve([]);
+          }
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+
+  getServiceJobs() {
+    return new Promise((resolve, reject) => {
+      import("../../rawdata/serviceJobs.json")
+        .then(data => {
+          debugger;
+          if (data && data.default) {
+            const validatedData = data.default.filter(item => {
+              return item.lat && item.lng;
+            });
+            resolve(validatedData);
+          } else {
+            resolve([]);
+          }
+        })
+        .catch(err => {
+          reject(err);
+        });
+    });
+  }
+
   async renderE5Engineers() {
+    const type = "e5Engineer";
     try {
-      const e5Engineers = await this.getE5Engineers();
-      console.log(e5Engineers);
-      if (e5Engineers && e5Engineers.length) {
+      const arrayContent = await this.getE5Engineers();
+      if (arrayContent && arrayContent.length) {
         let markers = [];
-        e5Engineers.forEach(item => {
+        arrayContent.forEach(item => {
           if (item.lat && item.lng) {
-            const marker = window.L.marker([item.lat, item.lng]).addTo(
-              this.mapElement
-            );
-            marker.bindPopup(`
-            <table class="popup-table">
-              <tr class="popup-table-row">
-                <td>Name</td>
-                <td>${item.FirstName}</td>
-              </tr>
-              <tr>
-                <td>Postcode</td>
-                <td>${item.MobileUserName}</td>
-              </tr>
-            </table>
-            `);
+            const marker = window.L.marker([item.lat, item.lng], {
+              icon: this.markerIcon(type)
+            }).addTo(this.mapElement);
+            marker.bindPopup(this.markerPopupContent(type, item));
             markers.push(marker);
           }
         });
         this.setState({
-          e5Engineers: markers
+          [type]: markers
         });
       } else {
-        throw new Error("No Engineers to be rendered");
+        throw new Error(`No ${type} to be rendered`);
       }
     } catch (err) {
       console.error(err);
-      alert(`Error while rendering E5 Engineers`);
+      alert(`Error while rendering ${type}`);
     }
   }
 
-  renderMarkers() {}
+  async renderBoilerJobs() {
+    const type = "boilerJob";
+    try {
+      const arrayContent = await this.getBoilerJobs();
+      if (arrayContent && arrayContent.length) {
+        let markers = [];
+        arrayContent.forEach(item => {
+          if (item.lat && item.lng) {
+            const marker = window.L.marker([item.lat, item.lng]).addTo(
+              this.mapElement
+            );
+            marker.bindPopup(this.markerPopupContent(type, item));
+            markers.push(marker);
+          }
+        });
+        this.setState({
+          [type]: markers
+        });
+      } else {
+        throw new Error(`No ${type} to be rendered`);
+      }
+    } catch (err) {
+      console.error(err);
+      alert(`Error while rendering ${type}`);
+    }
+  }
+
+  async renderServiceJobs() {
+    const type = "serviceJob";
+    try {
+      const arrayContent = await this.getServiceJobs();
+      console.log(arrayContent);
+      if (arrayContent && arrayContent.length) {
+        let markers = [];
+        arrayContent.forEach(item => {
+          if (item.lat && item.lng) {
+            const marker = window.L.marker([item.lat, item.lng], {
+              icon: this.markerIcon(type)
+            }).addTo(this.mapElement);
+            marker.bindPopup(this.markerPopupContent(type, item));
+            markers.push(marker);
+          }
+        });
+        this.setState({
+          [type]: markers
+        });
+      } else {
+        throw new Error(`No ${type} to be rendered`);
+      }
+    } catch (err) {
+      console.error(err);
+      alert(`Error while rendering ${type}`);
+    }
+  }
+
+  markerPopupContent(type, item) {
+    let content = "";
+    let tableContent = "";
+    Object.keys(item).forEach(k => {
+      tableContent =
+        tableContent +
+        `
+        <tr>
+          <td class="popup-table-row">
+              <td>${k}</td>
+              <td>${item[k]}</td>
+          </td>
+        </tr>
+      `;
+    });
+    switch (type) {
+      case "e5Engineer":
+        content = `
+        <table class="popup-table">
+         ${tableContent}
+        </table>
+        `;
+        break;
+      default:
+        //do nothing
+        break;
+    }
+    return content;
+  }
+
+  markerIcon(type) {
+    //   "https://res.cloudinary.com/razrlab/image/upload/c_scale,co_rgb:D12711,e_colorize:100,f_png/v1541359481/pin_t5dc4n.png",
+    let icon = "";
+    switch (type) {
+      case "e5Engineer":
+        icon =
+          "https://res.cloudinary.com/razrlab/image/upload/v1571935748/person-marker_wdvvbm.png";
+        break;
+      case "serviceJob":
+        icon =
+          "https://res.cloudinary.com/razrlab/image/upload/v1571935748/service-marker_tzrx0s.png";
+        break;
+      default:
+        //nothing
+        break;
+    }
+    const iconElement = window.L.icon({
+      iconUrl: icon,
+      iconSize: [25, 25],
+      iconAnchor: [25, 25]
+    });
+
+    return iconElement;
+  }
+
+  removeMarkers() {
+    return new Promise((resolve, reject) => {
+      let e5EngineerMarkers = this.state.e5Engineers;
+      if (e5EngineerMarkers && e5EngineerMarkers.length) {
+        e5EngineerMarkers.forEach(item => {
+          this.mapElement.removeLayer(item);
+        });
+      }
+    });
+  }
 
   onLoad() {
-    console.log(`came here`);
-
     const L = window.L;
     const $ = window.$;
     const config = window.config;
@@ -155,15 +305,14 @@ class MapComponent extends Component {
     loading.onAdd = function() {
       const div = L.DomUtil.create("div", "control loader");
 
-      div.innerHTML =
-        '\
-            <h4>Loading GeoJSON</h4>\
+      div.innerHTML = `
+            <h4>Loading GeoJSON</h4>
             <div>\
-                Areas... <span id="loadAreas">&#x21BB;</span><br>\
-                Districts... <span id="loadDistricts">&#x21BB;</span><br>\
-                Sectors... <span id="loadSectors">&#x21BB;</span>\
-            </div>\
-            ';
+                Areas... <span id="loadAreas">&#x21BB;</span><br>
+                Districts... <span id="loadDistricts">&#x21BB;</span><br>
+                Sectors... <span id="loadSectors">&#x21BB;</span>
+            </div>
+            `;
 
       return div;
     };
