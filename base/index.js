6ff24db7a7db2dac93acb5f558e250c4cd10c3c7
Zubair Ahmed <zubair1024@gmail.com>
2019-10-25 03:34:45 +0400

    adding cors and compression

--- base/index.js
+++ base/index.js
@@ -1,7 +1,27 @@
 const express = require("express");
+const cors = require("cors");
+const compression = require("compression");
+
+const PORT = (process.env.PORT = 4000);
+
+//global uncaughtException handler
+process.on("uncaughtException", function(error) {
+  if (error && error.stack) {
+    console.error("uncaughtException: " + error.stack);
+  } else {
+    console.error("uncaughtException: " + error);
+  }
+});
+//global unhandledRejection handler
+process.on("unhandledRejection", function(reason, p) {
+  console.error("unhandledRejection: " + reason);
+  if (reason && reason.stack) console.error(reason.stack);
+});
 
 const app = express();
-const PORT = (process.env.PORT = 4000);
+
+app.use(compression());
+app.use(cors);
 
 app.use(express.static(__dirname + "../app/build"));
 
