2bc4926cfd062df4ee37abaae94d3ae43161a663
Zubair <zubair1024@gmail.com>
2019-10-27 18:00:53 +0400

    for shipping

---
+++ app/build/precache-manifest.03c8f805ee4cd94beb1d65df6ee91df3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770792cda7f9438f21fb4c60a94d9c57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a557992b43b945665e0",
+    "url": "/static/css/main.1ec64012.chunk.css"
+  },
+  {
+    "revision": "c394917af71c33c6c82a",
+    "url": "/static/js/2.6bfc9c29.chunk.js"
+  },
+  {
+    "revision": "bec95f922a6cf1a0e1f3",
+    "url": "/static/js/3.b5c1d086.chunk.js"
+  },
+  {
+    "revision": "17720bbf589deffb8f33",
+    "url": "/static/js/4.40319e53.chunk.js"
+  },
+  {
+    "revision": "5e3055660e7cd99dca52",
+    "url": "/static/js/5.850028d9.chunk.js"
+  },
+  {
+    "revision": "08697d559bb9d4206fed",
+    "url": "/static/js/6.fe900f5f.chunk.js"
+  },
+  {
+    "revision": "1be338305f0fbf8d1218",
+    "url": "/static/js/7.9b914bbd.chunk.js"
+  },
+  {
+    "revision": "0a557992b43b945665e0",
+    "url": "/static/js/main.54a44fec.chunk.js"
+  },
+  {
+    "revision": "4378d69be61a00ed230d",
+    "url": "/static/js/runtime-main.c495f682.js"
+  }
+]);
