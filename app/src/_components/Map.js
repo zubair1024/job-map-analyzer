594673ced6f9cb5da4ba3e49e990c0640ee0e121
Zubair <zubair1024@gmail.com>
2019-10-28 20:46:10 +0400

    adding a sidebar

--- app/src/_components/Map.js
+++ app/src/_components/Map.js
@@ -7,6 +7,81 @@ class Map extends Component {
     return (
       <div>
         <div id="map"></div>
+        <div id="sidebar">
+          <h1>leaflet-sidebar</h1>
+
+          <p>
+            A responsive sidebar plugin for for{" "}
+            <a href="http://leafletjs.com/">Leaflet</a>, a JS library for
+            interactive maps.
+          </p>
+
+          <p>
+            <b>
+              Click on the marker to show the sidebar again when you've closed
+              it.
+            </b>
+          </p>
+
+          <p>Other examples:</p>
+
+          <ul>
+            <li>
+              <a href="listing-markers.html">listing-markers example</a>
+            </li>
+            <li>
+              <a href="two-sidebars.html">two-sidebars example</a>
+            </li>
+          </ul>
+
+          <p class="lorem">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+
+          <p class="lorem">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+
+          <p class="lorem">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+
+          <p class="lorem">
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet.
+          </p>
+        </div>
       </div>
     );
   }
@@ -485,6 +560,10 @@ class Map extends Component {
           <div class="checkbox">
             <label><input type="checkbox" value="boilerJobs">Boiler Jobs</label>
           </div>
+          <hr />
+          <div class="checkbox">
+            <label><input type="checkbox" value="geofenceControls">Geofence Controls</label>
+          </div>
         </div>
         `,
         classes: "panel panel-default",
@@ -532,6 +611,11 @@ class Map extends Component {
                   data.srcElement.checked
                     ? this.renderBoilerJobsHeatMap()
                     : this.removeBoilerJobsHeatMap();
+                  break;
+                case "geofenceControls":
+                  data.srcElement.checked
+                    ? this.showSideBar()
+                    : this.hideSideBar();
                   break;
                 default:
                   break;
@@ -590,6 +674,14 @@ class Map extends Component {
         layers: [layerOSM]
       });
 
+    const sidebar = L.control.sidebar("sidebar", {
+      closeButton: true,
+      position: "left"
+    });
+    map.addControl(sidebar);
+
+    //expose some elements
+    this.sidebarElement = sidebar;
     this.mapElement = map;
 
     // Add layers control
@@ -714,6 +806,14 @@ class Map extends Component {
 
       $("#loadSectors").html("&#x2717;");
     });
+  }
+
+  showSideBar() {
+    this.sidebarElement.show();
+  }
+
+  hideSideBar() {
+    this.sidebarElement.hide();
   }
 }
 
